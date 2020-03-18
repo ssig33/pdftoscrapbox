bd587885ba42c31f332574df6110cc109c47eba1
ssig33 <mail@ssig33.com>
2020-03-18 15:50:35 +0900

    bigger thumbnail and link to image

--- searchwithgyazo.user.js
+++ searchwithgyazo.user.js
@@ -2,7 +2,7 @@
 // @grant        GM_xmlhttpRequest
 // @name         Search with Gyazo
 // @namespace    http://tampermonkey.net/
-// @version      3.0
+// @version      4.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://scrapbox.io/*
@@ -19,30 +19,37 @@ const GM_get = (url)=>{
 (function() {
   'use strict';
 
-  const searchArticleAndAppend = async ({image_id, thumb_url}, urls)=>{
+  const searchArticleAndAppend = async ({image_id}, pages, lineIds)=>{
     const prj = location.pathname.split("/")[1];
     const url = `https://scrapbox.io/api/pages/${prj}/search/query?skip=0&sort=updated&limit=30&q=${image_id}`;
     const data = await (await fetch(url)).json();
     if(data.pages.length > 0){
-      data.pages.forEach((page)=>{
-        if(urls.indexOf(page.title) > -1) return true;
-        urls.push(page.title)
-        const li = jQuery(`
-          <li class="page-list-item list-style-item">
-            <a href="/${prj}/${encodeURIComponent(page.title)}" rel="route">
-              <div class="icon">
-                <img loading="lazy" src="${thumb_url}">
-              </div>
-              <div class="content">
-                <div class="title-with-description">${page.title}</div>
-                <div class="description"><span>${page.descriptions[0]}</span>
-              </div>
-              </div>
-            </a>
-          </li>
-        `);
-        jQuery('#searchWithGyazo').append(li);
-      })
+      for(const page of data.pages){
+        console.log(page);
+        if(!pages[page.title]){
+          pages[page.title] = await (await fetch(`https://scrapbox.io/api/pages/${prj}/${encodeURIComponent(page.title)}?followRename=true')`)).json();
+        }
+        const line = pages[page.title].lines.find(x => x.text.match(image_id));
+        if(lineIds.indexOf(line.id) === -1){
+          console.log('koko');
+          lineIds.push(line.id);
+          const li = jQuery(`
+            <li class="page-list-item list-style-item">
+              <a href="/${prj}/${encodeURIComponent(page.title)}#${line.id}" rel="route" style="display:flex" target='_blank'>
+                <div style="padding-right:20px">
+                  <img loading="lazy" src="http://gyazo.com/${image_id}/raw" style="width:250px;max-height:400px">
+                </div>
+                <div style="flex:1">
+                  <div class="title-with-description">${page.title}</div>
+                  <div class="description"><span>${page.descriptions[0]}</span>
+                </div>
+                </div>
+              </a>
+            </li>
+          `);
+          jQuery('#searchWithGyazo').append(li);
+        }
+      }
     }
   }
 
@@ -50,8 +57,9 @@ const GM_get = (url)=>{
     const query = decodeURIComponent(location.search.replace("?q=", ""))
     const url = `https://gyazo.com/api/internal/search_result?page=1&per=40&query=${encodeURIComponent(query)}`;
     const data = await GM_get(url);
-    const urls = [];
-    data.captures.forEach(cap => searchArticleAndAppend(cap, urls))
+    const pages = {};
+    const lineIds = [];
+    data.captures.forEach(cap => searchArticleAndAppend(cap, pages, lineIds))
     jQuery('#gyazobutton').remove();
   }
 
