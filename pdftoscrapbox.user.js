51ff9b77b8a327dfa164c4f8bf92c320cb184534
ssig33 <mail@ssig33.com>
2020-03-19 12:21:37 +0900

    version bump

--- pdftoscrapbox.user.js
+++ pdftoscrapbox.user.js
@@ -2,7 +2,7 @@
 // @grant        GM_xmlhttpRequest
 // @name         PDF to Scrapbox user.js
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      3.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://ssig33.github.io/pdftoscrapbox/*
